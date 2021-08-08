ad0403864a0c0e08b9c7ea39cbeafafe47388feb
Lagbjorn <likbjorn@yandex.ru>
2021-08-08 12:48:16 +0300

    Add DB query logging for dev environment

--- comments_for_strategia/config/settings/dev.py
+++ comments_for_strategia/config/settings/dev.py
@@ -21,3 +21,32 @@ MIDDLEWARE.extend(
 DEBUG_TOOLBAR_CONFIG = {
     'SHOW_TOOLBAR_CALLBACK': lambda request: DEBUG
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        }
+    },
+    'formatters': {
+        'default': {
+            'format': '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]',
+        },
+    },
+    'handlers': {
+        'debug-console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'default',
+            'filters': ['require_debug_true'],
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['debug-console'],
+            'propagate': False,
+        }
+    },
+}
